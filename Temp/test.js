883cc02e8b263e9ecbe9f385c5bbf00d030cf918
Odenari <odenari44@gmail.com>
2023-03-27 09:53:31 +0200

    recent changes to script: added few functions

--- Temp/test.js
+++ Temp/test.js
@@ -1,25 +1,69 @@
-const n = 'James';
+// const n = 'James';
 
-const person = {first: n};
+// const person = {first: n};
 
-console.log(person);
+// console.log(person);
 
-const sayHelloLinting = (fName) => {
-	console.log(`Hello linting, ${fName}`);
-};
-sayHelloLinting(n);
-sayHelloLinting('Anton');
+// const sayHelloLinting = (fName) => {
+// 	console.log(`Hello linting, ${fName}`);
+// };
+// sayHelloLinting(n);
+// sayHelloLinting('Anton');
 
-const storeName = 'Fikibolt';
-const storeDescription = {
-	budget: 10000,
-	employees: ['Budda', 'Krishna', 'Vishnu'],
-	products: {
-		chakra: 1000,
-		mana: 1000
-	},
-	open: 'close'
-};
+// const storeName = 'Fikibolt';
+// const storeDescription = {
+// 	budget: 10000,
+// 	employees: ['Budda', 'Krishna', 'Vishnu'],
+// 	products: {
+// 		chakra: 1000,
+// 		mana: 1000
+// 	},
+// 	open: 'close'
+// };
 
+// * вложенный цикл рисует треугольник из зведочек
+// let result = '';
+// let length = 7;
+// for(let i = 1; i < length; i++) {
+// 	for(let j = 0; j < i; j++) { //переменная j сбрасывается до ноля при каждой новой итерации верхнего охватывающего цикла 
+// 		result += '*';			 //внутренний цикл срабатывает каждый раз больше на один раз потому что i растет на 1  а j начинается с ноля
+// 		// console.log(j)
+// 	}
+// 	result += '\n';
+// }
+// console.log(result);
 
-console.log(typeof(NaN));
+// // * Тройной уровень вложенности - и метки
+// outer: for (let i = 0; i <= 4; i++) {
+// 	console.log(`First level: ${i}`);
+// 	for (let j = 0; j <=2; j++){
+// 		console.log(`Second level: ${j}`);
+// 		for( let k = 0; k < 2; k++) {
+// 			// if (k === 7) {continue outer;}
+// 			console.log(`Third level: ${k}`)
+// 		}
+// 	}
+// }
+
+//      *
+//     ***
+//    *****
+//   *******
+//  *********
+// ***********
+// ? Мог ли я решить это сам? Не знаю, но условие второго цикла пришлось подсмотреть =\
+let result = '';
+let length = 7;
+let spacesCount = 6;
+for(let i = 0; i < length; i++) {	
+	for(let k = 0; k <= spacesCount; k++) { 
+		result += ' ';
+	}
+	for(let j = 0; j < 2 * i + 1; j++) {
+		result += '*';
+	}
+	--spacesCount;
+	result += '\n';
+}
+console.log(result);
+
--- js/script.js
+++ js/script.js
@@ -1,23 +1,89 @@
 'use strict';
 
-/* eslint-disable no-unused-vars */
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+let numberOfFilms;
+function start() {
+	numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+		numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+	}
+}
+start();
+
 const personalMovieDB = {
 	count: numberOfFilms,
 	movies: {},
 	actors: {},
 	genres: [],
-	privat: false
+	private: false
 };
 
-const movieName = prompt('Один из просмотренных фильмов?', '');
-const movieRate =  prompt('Насколько его оцените?', '');
+// for (let i = 0; i < 2; i++) {
+// 	const movieName = prompt('Один из просмотренных фильмов?', ''),
+// 		movieRate =  prompt('Насколько его оцените?', '');
+// 	if (movieName != null && movieRate != null && movieName != '' && movieRate != '' && movieName.length < 50) {
+// 		personalMovieDB.movies[movieName] = movieRate;
+// 		console.log('done');
+// 	} else {
+// 		console.log('error');
+// 		i--;
+// 	}
+// }
 
-const movieName_1 = prompt('Один из просмотренных фильмов?', '');
-const movieRate_1 =  prompt('Насколько его оцените?', '');
+// let promptCounter = 0;
+// while (promptCounter < 3) {
+// 	const movieName = prompt('Один из просмотренных фильмов?', ''),
+// 		movieRate = prompt('Насколько его оцените?');
+// 	if (movieName != null && movieRate != null && movieName != '' && movieRate != '' && movieName.length < 50) {
+// 		personalMovieDB.movies[movieName] = movieRate;
+// 		console.log('done');
+// 		promptCounter++;
+// 	} else {
+// 		promptCounter--;
+// 		globalThis.alert('Введите корректные значения');
+// 	}
+// }
 
-personalMovieDB.movies[movieName] = movieRate;
-personalMovieDB.movies[movieName_1] = movieRate_1;
 
-console.log(personalMovieDB);
+function rememberUserFilms() {
+	let counter = 0;
+	start: do {
+		const movieName = prompt('Один из просмотренных фильмов?', ''),
+			movieRate = prompt('Насколько его оцените?');
+		if (movieName != null && movieRate != null && movieName != '' && movieRate != '' && movieName.length < 50) {
+			personalMovieDB.movies[movieName] = movieRate;
+			console.log('done');
+			counter++;
+		} else {
+			globalThis.alert('Введите корректные значения');
+			counter--;
+			continue start;
+		}	
+	} while (counter < 2);
+}
+rememberUserFilms();
+
+function userLvl() {
+	if (personalMovieDB.count < 10) {
+		console.log('Просмотрено довольно мало фильмов');
+	} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+		console.log('Вы классический зритель');
+	} else if (personalMovieDB.count >=  30) {
+		console.log('Вы киноман');
+	} else {console.log('Произошла ошибка!');}
+}
+userLvl();
+
+function showMyDB(hidden) {
+	if(!hidden) {
+		console.log(personalMovieDB);
+	}
+}
+showMyDB(personalMovieDB.private);
+
+function userGenres() {
+	for (let i = 1; i < 4; i++) {
+		personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+	}
+}
+userGenres();
