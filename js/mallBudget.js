b5936ea6369c2866ac12cf7ebd30cd6829dc82b8
Odenari <odenari44@gmail.com>
2023-04-05 16:46:35 +0200

    resolve task with additional lvl of dificulty (*star lvl)

--- js/mallBudget.js
+++ js/mallBudget.js
@@ -19,29 +19,38 @@ const shoppingMallData = {
 	],
 	height: 5,
 	moneyPer1m3: 30,
-	budget: 50000,
-	mallAreaCalc: function(shops) {
+	budget: 0000,
+	// mallAreaCalc: function(shops) {
+	// 	let area = 0;
+	// 	shops.forEach(({width, length}) => {
+	// 		area += width * length;
+	// 	});
+	// 	return this.mallArea = area;
+	// },
+	// volCalc: function(mallArea, height) {
+	// 	return this.volume = height * mallArea;
+	// },
+	// isBudgetEnough: function() {
+
+	// 	if (this.budget / this.volume >= 0) {
+	// 		return 'Бюджета достаточно';
+	// 	} else {
+	// 		return 'Бюджета недостаточно';
+	// 	}
+	// }
+	isBudgetEnough: function(data) {
 		let area = 0;
-		shops.forEach(({width, length}) => {
+		let volume = 0;
+		let isEnough= 0;
+		data.shops.forEach(({width, length}) => {
 			area += width * length;
 		});
-		return this.mallArea = area;
-	},
-	volCalc: function(mallArea, height) {
-		return this.volume = height * mallArea;
-	},
-	isBudgetEnough: function() {
-
-		if (this.budget / this.volume >= 0) {
+		volume = data.height * area;
+		isEnough = data.moneyPer1m3 * volume;
+		if (data.budget >= isEnough) {
 			return 'Бюджета достаточно';
 		} else {
 			return 'Бюджета недостаточно';
 		}
 	}
 };
-shoppingMallData.mallAreaCalc(shoppingMallData.shops);
-console.log(shoppingMallData.mallArea);
-shoppingMallData.volCalc(shoppingMallData.mallArea, shoppingMallData.height);
-console.log(shoppingMallData.volume);
-const theAnswer = shoppingMallData.isBudgetEnough();
-console.log(theAnswer);
